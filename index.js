3ee699d642a1986719a389dd082eb73286532eaa
Thibault Lesuisse <lesuisse.thibault@gmail.com>
2019-03-03 20:28:05 +0100

    chreo(): connect to database

--- index.js
+++ index.js
@@ -1,26 +1,91 @@
+// load environment variables
+require('dotenv').config();
+const fs = require('fs');
 const https = require('https');
-const APIKEY = "efff9a418c9b8ff16056850bd62e8caf";
-let options = {
-    headers: {
-        Accept: 'application/json',
-        Authorization: `Bearer ${APIKEY}`
+const cron = require('node-cron');
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://mongo';
+const client = new MongoClient(url);
+
+collectData();
+// Collect data every 20 seconds
+//cron.schedule('*/20 * * * * *', collectData);
+
+
+
+let date = new Date();
+
+
+
+function collectData () {
+    const APIKEY = process.env.MIVB_API_KEY;
+    let options = {
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${APIKEY}`
+        }
     }
+
+    fetchData("https://opendata-api.stib-mivb.be/NetworkDescription/1.0/PointByLine/39", options, false).then((stops) => {
+    let line_ids = []
+    stops.lines.forEach(element => {
+        element.points.forEach(id => {
+            line_ids.push(id.id)
+        })
+    });
+
+    let lines_ids_request = "";
+    let promises = [];
+    for(let i = 0;i<line_ids.length;i++){
+        lines_ids_request = lines_ids_request.concat("," + line_ids[i]);
+        if( i%10 == 0){
+            //remove first ,
+            lines_ids_request = lines_ids_request.slice(1);            
+            promises.push(fetchData(`https://opendata-api.stib-mivb.be/OperationMonitoring/3.0/PassingTimeByPoint/${encodeURIComponent(String(lines_ids_request))}`, options, true));
+            lines_ids_request = ""
+        }
+    }
+    // Wait for all the promises to resolve
+    Promise.all(promises).then( data => {
+        client.connect((err) => {
+            const db = client.db("MIVB");
+            let collection = db.collection("MIVB");
+            collection.insert(data, (error, result) => {
+                if(error)console.error("Error while inserting into the database")
+                console.log(result)
+            })
+        })
+        fs.writeFile(`files/${date.getTime() + ".json"}`, JSON.stringify(data) , function(err) {
+            if(err) {
+                return console.log(err);
+            }    
+        }); 
+    })
+
+}).catch((e) => {
+    console.error(e);
+    //handle error
+});
 }
 
-https.get("https://opendata-api.stib-mivb.be/OperationMonitoring/3.0/PassingTimeByPoint/5528", options, (res) => { 
-    let position;
-    let date = new Date();
 
-    res.on('data', (d) => {
-               position += d;
+function fetchData(url, options, timestamp) {
+    return new Promise((resolve, reject) => {
+        https.get(url, options, (res) => {
+            let position = "";
+
+            res.on('data', (d) => {
+                position += d;
             });
-    res.on('end', () => {
-        position["time"] = date.getHours();
-        console.log(date.getHours())
-        console.log(position)
+            res.on('end', () => {
+                let parsed_position = JSON.parse(position);
+                if (timestamp) parsed_position["time"] = date.getTime();
+                resolve(parsed_position)
+            })
+
+        }).on('error', (e) => {
+            console.error(e);
+            reject(e);
+        });
     })
-            
-}).on('error', (e) => {
-    console.error(e);
-});
- 
+}
