36f4fab7952233af5beb9fc9675b4eb42e22ad1d
Thibault <lesuisse.thibault@gmail.com>
2019-03-21 10:29:09 +0100

    chore(): tried catching results

--- gtfs_parser.js
+++ gtfs_parser.js
@@ -4,7 +4,7 @@ let stoptimes = [];
 
 // REGEXS
 const TRIPS_REGEX = /^(?:17),(?:\d{9}),(\d{18})/gm;
-const STOP_TIMES_REGEX = /^(\d{18})/gm;
+const STOP_TIMES_REGEX = /^(\d{18}),(\d{2}:\d{2}:\d{2}),(\d{2}:\d{2}:\d{2}),([^,]{2,})/gm;
 
 fs.readFile("./gtfs/trips.txt","utf8", (err, data) => {
     if(err)console.error(err);
@@ -17,12 +17,22 @@ fs.readFile("./gtfs/trips.txt","utf8", (err, data) => {
     fs.readFile("./gtfs/stop_times.txt", "utf-8", (err, data_stoptimes) => {
         if(err)console.error(err);
         match = STOP_TIMES_REGEX.exec(data_stoptimes);
+        let old_match;
         while(match != null){
             if(match[1] != null && trips.indexOf(match[1])!= -1){
-                stoptimes.push(match[1]);
-            }
+                //Here I need the second group from the regular expression which contains the times
+                if(match[1] == old_match){
+                    stoptimes[stoptimes.length - 1].timetable.push({ "arrival_time": match[2], "departure_time" : match[3], "stop_id" : match[4]});
+                }else {
+                    stoptimes.push({"trip": match[1], timetable: [{ "arrival_time": match[2], "departure_time" : match[3], "stop_id" : match[4]}]});
+                }
+                old_match = match[1];
+            }   
             match = STOP_TIMES_REGEX.exec(data_stoptimes)
         }
-        console.log(stoptimes);
+        // Save everything in a file.
+        fs.writeFile("./files/39.json", JSON.stringify(stoptimes), (err) => {
+            if(err)console.log("Error while writing resulting json file.");
+        })
     })
 })
