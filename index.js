650c08c4090efea73489c86a8edef48ca6caf709
Thibault <lesuisse.thibault@gmail.com>
2019-03-27 15:24:35 +0100

    bug(): fixed failing JSON parse

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 
 //Collect data every 20 seconds
-cron.schedule('*/20 * * * * *', collectData); 
+cron.schedule('*/20 * * * * *', collectData);
 
 // Database credentials
 const user = encodeURIComponent(process.env.MONGO_USERNAME);
@@ -19,7 +19,10 @@ client.connect((err) => {
     db = client.db("MIVB");
 });
 
-function collectData () {
+//Global arrays
+let line_ids = []
+
+function collectData() {
     const APIKEY = process.env.MIVB_API_KEY;
     let options = {
         headers: {
@@ -27,39 +30,41 @@ function collectData () {
             Authorization: `Bearer ${APIKEY}`
         }
     }
+    if (!Array.length > 0) {
 
-    fetchData("https://opendata-api.stib-mivb.be/NetworkDescription/1.0/PointByLine/39", options, false).then((stops) => {
-    let line_ids = []
-    stops.lines.forEach(element => {
-        element.points.forEach(id => {
-            line_ids.push(id.id)
-        })
-    });
-
-    let lines_ids_request = "";
-    let promises = [];
-    for(let i = 0;i<line_ids.length;i++){
-        lines_ids_request = lines_ids_request.concat("," + line_ids[i]);
-        if( i%10 == 0){
-            //remove first ,
-            lines_ids_request = lines_ids_request.slice(1);            
-            promises.push(fetchData(`https://opendata-api.stib-mivb.be/OperationMonitoring/3.0/PassingTimeByPoint/${encodeURIComponent(String(lines_ids_request))}`, options, true));
-            lines_ids_request = ""
-        }
     }
-    // Wait for all the promises to resolve
-    Promise.all(promises).then( data => {
+    fetchData("https://opendata-api.stib-mivb.be/NetworkDescription/1.0/PointByLine/39", options, false).then((stops) => {
+
+        stops.lines.forEach(element => {
+            element.points.forEach(id => {
+                line_ids.push(id.id)
+            })
+        });
+
+        let lines_ids_request = "";
+        let promises = [];
+        for (let i = 0; i < line_ids.length; i++) {
+            lines_ids_request = lines_ids_request.concat("," + line_ids[i]);
+            if (i % 10 == 0) {
+                //remove first ,
+                lines_ids_request = lines_ids_request.slice(1);
+                promises.push(fetchData(`https://opendata-api.stib-mivb.be/OperationMonitoring/3.0/PassingTimeByPoint/${encodeURIComponent(String(lines_ids_request))}`, options, true));
+                lines_ids_request = ""
+            }
+        }
+        // Wait for all the promises to resolve
+        Promise.all(promises).then(data => {
             let collection = db.collection("MIVB");
             collection.insertMany(data, (error, result) => {
-                if(error)console.error("Error while inserting into the database")
+                if (error) console.error("Error while inserting into the database")
             })
-        }).catch( (err) => {
+        }).catch((err) => {
             console.log("(Promise.all(): Failed to get results)");
         })
-}).catch((e) => {
-    console.error(e);
-    //handle error
-});
+    }).catch((e) => {
+        console.error(e);
+        //handle error
+    });
 }
 
 
@@ -72,12 +77,17 @@ function fetchData(url, options, timestamp) {
                 position += d;
             });
             res.on('end', () => {
-                let parsed_position = JSON.parse(position);
-                if (timestamp) {
-                    let date = new Date();
-                    parsed_position["time"] = date.getTime();
+                try {
+                    let parsed_position = JSON.parse(position);
+                    if (timestamp) {
+                        let date = new Date();
+                        parsed_position["time"] = date.getTime();
+                    }
+                    resolve(parsed_position)
+                } catch (error) {
+                    console.error(error)   
                 }
-                resolve(parsed_position)
+             
             })
 
         }).on('error', (e) => {
